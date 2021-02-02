11a4e17512dc09a10e710e2a666d20208446636e
Stuart Sears <stuart@sjsears.com>
2021-02-02 15:56:33 +0000

    much blackness, plus extension register updates

--- ukedown/translations.py
+++ ukedown/translations.py
@@ -20,21 +20,20 @@
 
 
 WP_JUNK = {
-        # first hyphen characters, there are a few and wordprocessors do love 'em
-        '\u2010': '-', # hyphen
-        '\u2011': '-', # non-breaking hyphen
-        '\u2012': '-', # figure-dash
-        '\u2013': '-', # en-dash
-        '\u2014': '-', # em-dash
-        '\u2015': '-', # horizontal bar
-        # then the silly pretty quote things): let's just use the normal ascii ones
-        '\u2018': "'", # left single quotation
-        '\u2019': "'", # right single quotation
-        '\u201c': '"', # left double quotation
-        '\u201d': '"', # right double quotation
-        '\u2026': '...', # ellipsis
-        }
+    # first hyphen characters, there are a few and wordprocessors do love 'em
+    "\u2010": "-",  # hyphen
+    "\u2011": "-",  # non-breaking hyphen
+    "\u2012": "-",  # figure-dash
+    "\u2013": "-",  # en-dash
+    "\u2014": "-",  # em-dash
+    "\u2015": "-",  # horizontal bar
+    # then the silly pretty quote things): let's just use the normal ascii ones
+    "\u2018": "'",  # left single quotation
+    "\u2019": "'",  # right single quotation
+    "\u201c": '"',  # left double quotation
+    "\u201d": '"',  # right double quotation
+    "\u2026": "...",  # ellipsis
+}
 
 # unicode strings have their own 'translate' method so we just need a table:
-UNICODE_CLEAN_TABLE = dict((ord(k), v) for k,v in list(WP_JUNK.items()))
-
+UNICODE_CLEAN_TABLE = dict((ord(k), v) for k, v in list(WP_JUNK.items()))
